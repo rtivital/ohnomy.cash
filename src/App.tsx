1f4ca31bacde75c783829f587e5a29523acf6f12
Vitaly Rtishchev <rtivital@gmail.com>
2021-01-04 17:05:39 +0300

    Add example with authentication

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import axios from 'axios';
 
 export default function App() {
   useEffect(() => {
-    axios.get('http://localhost:4001/').then(console.log);
+    axios
+      .get('http://localhost:4001/spendings?month=2021-01', {
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJ0aXZpdGFsQGdtYWlsLmNvbSIsImlkIjoiNWZmMmZjOWZlMmYwZTYzZTU3NDFkZTdhIiwiaWF0IjoxNjA5NzY3MTg4fQ.KwouiAhFl3RGD_LMAvoof9KlXbihGCU_uYwW1RbKDSw',
+        },
+      })
+      .then(console.log);
   }, []);
   return <div>App</div>;
 }
