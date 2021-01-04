69644c81938361ada41a1cf66512aec3953b4e32
Vitaly Rtishchev <rtivital@gmail.com>
2021-01-04 17:01:26 +0300

    Add fetch example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  useEffect(() => {
+    axios.get('http://localhost:4001/').then(console.log);
+  }, []);
   return <div>App</div>;
 }
