d83aeca9a82005012cf60c0b1ce9b65dbfcf68be
Vitaly Rtishchev <rtivital@gmail.com>
2021-01-07 16:40:57 +0300

    Add fetching example

---
+++ src/components/routes/home/HomePage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+
+axios.get('http://localhost:4005/categories', {
+  headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZjZjMmY4MjJlOTk1MjI5Y2ZiZWEzOCIsIm5hbWUiOiLQktC40YLQsNC70LjQuSIsImVtYWlsIjoicnRpdml0YWxAZ21haWwuY29tIiwiaWF0IjoxNjEwMDA3Mjg5LCJleHAiOjE2MTAzNjcyODl9.ZR_3_7RZ1FFs-QnJ4xEcxGXPvMcWdzGUNztcS_nxQS8',
+  },
+});
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: 50, fontWeight: 'bold', textAlign: 'center' }}>
+        React Pages Boilerplate!
+      </h1>
+    </div>
+  );
+}
