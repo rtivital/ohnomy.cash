0db9bdeba6ba7fd11f8cc39b0159bc639f2e8a76
Vitaly Rtishchev <rtivital@gmail.com>
2021-01-13 21:01:12 +0300

    Add all api types

--- src/api/types.ts
+++ src/api/types.ts
@@ -4,3 +4,18 @@ export interface Month {
   balance: number;
   savings: number;
 }
+
+export interface Category {
+  id: string;
+  name: string;
+  color: string;
+}
+
+export interface Transaction {
+  id: string;
+  category: Category;
+  type: 'saving' | 'income' | 'spending';
+  amount: number;
+  date: string;
+  description: string;
+}
