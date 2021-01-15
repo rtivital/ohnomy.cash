c65dbf4c0d530e0f5e6165ac2dffb9a5922a1e3a
Vitaly Rtishchev <rtivital@gmail.com>
2021-01-15 12:38:12 +0300

    Add props mapping to category picker

--- src/routes/month/Spendings/CategoryPicker.tsx
+++ src/routes/month/Spendings/CategoryPicker.tsx
@@ -1,4 +1,57 @@
+import oc from 'open-color';
 import React from 'react';
-import { TagPicker } from '@mantine/tag-picker';
+import { nanoid } from 'nanoid';
+import { TagPicker, TagPickerTag } from '@mantine/tag-picker';
+import useTranslations from 'src/translations/use-translations';
 
-export default function CategoryPicker() {}
+export const OPEN_COLOR_THEMES = [
+  'red',
+  'pink',
+  'grape',
+  'violet',
+  'indigo',
+  'blue',
+  'cyan',
+  'teal',
+  'green',
+  'lime',
+  'yellow',
+  'orange',
+] as const;
+
+interface CategoryPickerProps {
+  data: TagPickerTag[];
+  value: TagPickerTag;
+  onChange(value: TagPickerTag): void;
+  onCategoryCreate(values: TagPickerTag): void;
+  onCategoryDelete(id: string): void;
+  onCategoryUpdate(id: string, values: Omit<TagPickerTag, 'id'>): void;
+}
+
+export default function CategoryPicker({
+  value,
+  onChange,
+  data,
+  onCategoryCreate,
+  onCategoryUpdate,
+  onCategoryDelete,
+}: CategoryPickerProps) {
+  const t = useTranslations();
+
+  return (
+    <TagPicker
+      value={value}
+      onChange={onChange}
+      data={data}
+      searchPlaceholder={t('search_categories')}
+      colors={OPEN_COLOR_THEMES.map((theme) => ({ name: t(theme), color: oc[theme][0] }))}
+      description={t('select_or_create_category')}
+      createLabel={`+ ${t('create_category')}`}
+      deleteLabel={t('delete_category')}
+      noValueLabel={t('category_not_selected')}
+      onTagCreate={(values) => onCategoryCreate({ ...values, id: `category-created-${nanoid()}` })}
+      onTagDelete={onCategoryDelete}
+      onTagUpdate={onCategoryUpdate}
+    />
+  );
+}
